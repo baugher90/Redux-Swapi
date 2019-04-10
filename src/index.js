f8fba41f6e1040e3f387e4707a0771fe0add454e
baugher90 <45408433+baugher90@users.noreply.github.com>
2019-04-10 17:20:02 -0400

    actions added

--- src/index.js
+++ src/index.js
@@ -15,7 +15,8 @@ import rootReducer from "./reducers";
 
 const store = createStore(
   rootReducer,
- applyMiddleware(thunk, logger)
+ applyMiddleware(thunk, logger),
+ window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 ReactDOM.render(
